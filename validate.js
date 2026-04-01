4861db19ce72949a502b8b33315808b7eb94eb87
Supisara <tivaratkuls@gmail.com>
2026-04-01 13:56:44 +0700

    validate form

--- validate.js
+++ validate.js
@@ -17,19 +17,11 @@ function checkTicketNo() {
 }
 
 function validateForm(){
-	if(!checkNID()){
-	  alert("Invalid value for National ID!");
-	  document.getElementById("nid").focus();
-	  return false;
-	}else{
-		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
-		  document.getElementById("ticknum").focus();
-		  return false;
-		}else{
-			total = priceCalculate();
-			alert("Total price for this booking is "+total+" USD");
-			return false;
-		}
-	}
+    let fname = document.getElementById("fname").value.trim();
+    let lname = document.getElementById("lname").value.trim();
+
+    if(fname === "" || lname === ""){
+        alert("Please fill in all required fields!");
+        return false;
+    }
 }
